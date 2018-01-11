63237db8c6b0e57117e95f79cdf65edd6ccde8e8
robinBanquo <caramaschi@hotmail.com>
2018-01-12 00:08:25 +0100

    ajout de nouvelles section ok! a commenter

--- app/controllers/EditController.php
+++ app/controllers/EditController.php
@@ -7,7 +7,6 @@
 class EditController extends Controller {
 	// on fait en sorte que toutes les methodes ne soient accecssibles que si l'utilisateur est admin,
 	//sinon, on le renvoie vers la page de login
-	//todo : verifier que ca marche bien sinon c'est une faille de sécu
 	function beforeroute() {
 		if (!$this->isAdmin()) {
 			//on rajoute un petit message d'erreur qui sera affiché sur la page de login
@@ -21,29 +20,103 @@ class EditController extends Controller {
 
 
 	//méthode d'insertion d'une section dans la page
+	//TODO CRSF
 	function addSection() {
+		//on commence par récuperer les valeurs contenues dans le formulaire
 		$reference=$this->f3->get('POST.reference');
 		$positionReference=$this->f3->get('POST.positionReference');
 		$module=$this->f3->get('POST.module');
-
-		$siteStructure = $this->getSiteStructure();
-		$newSiteStructure = array();
-		foreach ($siteStructure as $i=>$section){
-			if($section['id']===$reference){
-				$newSection = array('id' => uniqid(),'module'=>$module );
-				if($positionReference=== "after"){
+		//on recupere la structure du site
+		$siteStructure=$this->getSiteStructure();
+		//on cree un array pour acceuillir la nouvelle structure
+		$newSiteStructure=array();
+		//on vient créer un id pour cette section et lui donner aussi son module appelé
+		$newSection=array('id'=>uniqid(),'module'=>$module);
+		//pour chaque element de l'anciene structure
+		foreach ($siteStructure as $i=>$section) {
+			//si on est sur la section réference pour l'ajout
+			if ($section['id']===$reference) {
+				//on verifie ensuite si il faut placer la nouvelle section
+				// avant ou apres la section de réference
+				if ($positionReference==="after") {
+					//si c'est apres, on push d'abord la section de réference avant la nouvelle section
 					array_push($newSiteStructure,$section);
 					array_push($newSiteStructure,$newSection);
-				}else{
+				} else {
+					//si c'est avant, on push d'abord la la nouvelle section avant section de réference
 					array_push($newSiteStructure,$newSection);
 					array_push($newSiteStructure,$section);
 				}
-			}else{
+			} else {//dans tout les autres cas on push la section au tableau de resultat
 				array_push($newSiteStructure,$section);
 			}
 		}
+		//puis on sauvegarde en base la nouvelle structure
 		$this->db->write('siteStructure.json',$newSiteStructure);
+		//et on reroute vers la page d'admin
+		$this->f3->reroute('/admin');
+	}
 
+	//méthode de supression d'une section dans la page
+	//TODO CRSF
+	function deleteSection() {
+		//in recupere l'id de la section a supprimer dans les query parameters
+		$moduleId=$this->f3->get('GET.id');
+		//on recupere la structure du site
+		$siteStructure=$this->getSiteStructure();
+		//on viens supprimer la section avec l'id correspondant
+		foreach ($siteStructure as $i=>$section) {
+			if ($section['id']===$moduleId) {
+				array_splice($siteStructure,$i,1);
+			}
+		}
+		//on sauvegarde en base
+		$this->db->write('siteStructure.json',$siteStructure);
+		//et on reroute vers la page d'admin
+		$this->f3->reroute('/admin');
+	}
+
+	//méthode de déplacement vers le haut d'une section de la page
+	function moveUpSection() {
+		//in recupere l'id de la section a supprimer dans les query parameters
+		$moduleId=$this->f3->get('GET.id');
+		//on recupere la structure du site
+		$siteStructure=$this->getSiteStructure();
+		//on boucle sur la structure
+		foreach ($siteStructure as $i=>$section) {
+			//lorqu'on trouve le bon module
+			if ($section['id']===$moduleId) {
+				if ($i>0) {//on le déplace vers le haut(sauf si c'etait pas déja le premier)
+					$el=$siteStructure[$i-1];
+					$siteStructure[$i-1]=$siteStructure[$i];
+					$siteStructure[$i]=$el;
+				}
+			}
+		}
+		//on sauvegarde
+		$this->db->write('siteStructure.json',$siteStructure);
+		//et on reroute vers la page d'admin
+		$this->f3->reroute('/admin');
+	}
+
+	//méthode de supression d'une section dans la page
+	function moveDownSection() {
+		//in recupere l'id de la section a supprimer dans les query parameters
+		$moduleId=$this->f3->get('GET.id');
+		//on recupere la structure du site
+		$siteStructure=$this->getSiteStructure();
+		//on boucle sur la structure jusqu'a trouver notre module
+		foreach ($siteStructure as $i=>$section) {
+			if ($section['id']===$moduleId) {
+				if ($i<count($siteStructure)-1) {//et on le descends dans le tableau si c'est pas le dernier element
+					$el=$siteStructure[$i+1];
+					$siteStructure[$i+1]=$siteStructure[$i];
+					$siteStructure[$i]=$el;
+				}
+			}
+		}
+		//on sauvegarde
+		$this->db->write('siteStructure.json',$siteStructure);
 		//et on reroute vers la page d'admin
 		$this->f3->reroute('/admin');
 	}
--- app/views/base/editBtns/editBtns.js
+++ app/views/base/editBtns/editBtns.js
@@ -2,15 +2,21 @@ $(document).ready(function(){
 // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
 	$('#addSectionModal').modal();
 
+	//au click sur inerer une section
 	$('.addSectionBtn').click(function () {
+		//on récupere toutes les infos stockées dans l'id du boutton
 		let splittedId = this.id.split("-")
+		//et on remplit le formulaire caché avec
 		$('#addSectionForm input[name = positionReference]').val(splittedId[1])
 		$('#addSectionForm input[name = reference]').val(splittedId[2])
 	})
 
+	//au click sur le le module choisi
 	$('.chooseModuleBtn').click(function () {
+		//on viens récurer le nom du module choisi pour le rentrer dans le formulaire
 		$('#addSectionForm input[name = module]').val(this.id.split("-")[1])
-		setTimeout(function () {
+		setTimeout(function () {//on attends un peu pour etre sur que c'est fait
+			//et on soumet le formulaire
 			$('#addSectionForm').submit()
 		},50)
 	})
